8e3c32b23ce45963d2bee07e4ac456a01b60d932
Duncan Parkes <deparkes@gmail.com>
2022-07-16 16:57:18 +0000

    Add exception handling for ble data load

--- ble2db.py
+++ ble2db.py
@@ -2,22 +2,24 @@ import json
 import sqlite3
 from datetime import datetime
 
-with open('test.file', 'r') as f:
-    last_line = f.readlines()[-1]
+try:
+    with open('test.file', 'r') as f:
+        last_line = f.readlines()[-1]
 
-last_line = last_line.strip("Temperature info").replace("'", '"')
+        last_line = last_line.strip("Temperature info").replace("'", '"')
 
-json_line = json.loads(last_line)
+        json_line = json.loads(last_line)
 
-conn = sqlite3.connect("co2meter.db")
-cursor = conn.cursor()
+        conn = sqlite3.connect("co2meter.db")
+        cursor = conn.cursor()
 
-cursor.executemany("INSERT INTO data VALUES (?,?,?,?)",
-        [(datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            None,
-            json_line['temperature'],
-            2)
-            ]
-        )
-conn.commit()
-
+        cursor.executemany("INSERT INTO data VALUES (?,?,?,?)",
+                [(datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                    None,
+                    json_line['temperature'],
+                    2)
+                 ]
+                )
+        conn.commit()
+except:
+    print("Unable to load ble data")
