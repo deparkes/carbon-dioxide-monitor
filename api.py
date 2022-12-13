c3161c8652d94a9a97ce634087c7caf8ddc62ecd
Duncan Parkes <deparkes@gmail.com>
2022-12-13 17:04:24 +0000

    Add v2 of api

--- api.py
+++ api.py
@@ -18,12 +18,26 @@ def get_data():
     statement = "SELECT * FROM data"
     cursor.execute(statement)
     return cursor.fetchall()
-    
+
+def get_data_multi_sensor():
+    db = get_db()
+    cursor = db.cursor()
+    statement = "SELECT * FROM data_multi_sensor"
+    cursor.execute(statement)
+    return cursor.fetchall()
+
 @app.route('/api')
 def api():
     data = get_data()
     columns = ['timestamp', 'co2', 'temperature', 'deviceid']
     result =  [{'timestamp': row[0], 'co2': row[1], 'temperature': row[2], 'deviceid': row[3]} for row in data]    
+    return jsonify(result)
+
+@app.route('/api/v2')
+def api_v2():
+    data = get_data_multi_sensor()
+    columns = ['timestamp', 'deviceid', 'key', 'value']
+    result =  [{'timestamp': row[0], 'deviceid': row[1], 'key': row[2], 'value': row[3]} for row in data]    
     return jsonify(result)
 
 @app.route('/')
